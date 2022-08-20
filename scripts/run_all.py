22bf4c12c8e7c0ced38d8781e69304601a251341
djeada <addjellouli1@gmail.com>
2022-08-20 12:18:32 +0200

    Merge branch 'master' of https://github.com/djeada/Personal-Website

---
+++ scripts/run_all.py
@@ -0,0 +1,24 @@
+'''
+Used to run all the specified scripts with provided command line arguments.
+'''
+
+import subprocess
+
+SCRIPTS_TO_ARGS = {'apply_common_elements.py': ['-i ..\src\index.html -o ..\src\index.html'],}
+
+def run_all():
+    '''
+    Runs all the scripts with provided command line arguments.
+    '''
+    for script, args_list in SCRIPTS_TO_ARGS.items():
+        print('Running {}'.format(script))
+        for args in args_list:
+            print('Arguments: {}'.format(args))
+            print('----------------------------------------------------')
+            subprocess.call([script, args])
+            print('----------------------------------------------------')
+            print()
+    
+if __name__ == '__main__':
+    run_all()
+    print('Done!')
